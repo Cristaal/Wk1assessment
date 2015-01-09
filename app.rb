35359025e731106847071afac341ad3e45e47c00
Cristaal <crystalcontrerasgrossman@gmail.com>
2015-01-09 12:02:32 -0800

    Views file with folders

--- app.rb
+++ app.rb
@@ -1,10 +1,15 @@
 require('sinatra')
 require('sinatra/reloader')
-gem('sinatra-contrib')
-
 require('./lib/wordfreq')
 
 
-get('/form') do
-  "test"
+get('/') do
+  erb(:form)
+end
+
+get('/result') do
+  @sentence = params.fetch("first_sentence")
+  @search = params.fetch("first_sentence").wordfreq("search_word")
+  @result = params.fetch("first_sentence").wordfreq("search_word")
+  erb(:result)
 end
