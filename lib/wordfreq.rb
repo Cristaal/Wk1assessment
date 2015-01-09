b7e5843cf19aa2a84057102319d0f00d3dc53145
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-01-08 22:22:58 -0800

    Remove special characters from string

--- lib/wordfreq.rb
+++ lib/wordfreq.rb
@@ -1,6 +1,7 @@
 class String
   define_method(:wordfreq) do |search_word|
-    word_list = self.split(" ",)
+    stripped_string = self.gsub(/[^a-zA-Z. ]/, ' ')
+    word_list = stripped_string.split(" ")
     word_list.count(search_word)
    end
 end
