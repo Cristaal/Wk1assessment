bf4a21c939de764ad53e58bb94f65e8463a3cb8b
Cristaal <crystalcontrerasgrossman@gmail.com>
2015-02-22 00:08:45 -0800

    add updated app.rb to account for empty form fields

--- app.rb
+++ app.rb
@@ -11,5 +11,10 @@ get('/result') do
   @first_sentence = params.fetch("first_sentence")
   @search_word = params.fetch("search_word")
   @result = params.fetch("first_sentence").wordfreq(params.fetch("search_word"))
+    if @first_sentence.empty? || @search_word.empty?
+      @result = "Please enter a value"
+    else
+      @result
+    end
   erb(:result)
 end
